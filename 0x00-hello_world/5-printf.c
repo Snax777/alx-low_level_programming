ba979ebccec1e322636dbfb7a2d0782189fde972
Snax777 <sinethembatompelo3@gmail.com>
2023-02-16 18:22:28 -0800

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,6 +3,6 @@
 /* Using printf */
 int main();
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
